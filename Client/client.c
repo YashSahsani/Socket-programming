0b4aa038e59e9c4de491bee4a68897577364d1e8
YashSahsani <yashsahsani5@gmail.com>
2024-04-08 14:21:04 -0400

    feat: date logic

--- Client/client.c
+++ Client/client.c
@@ -42,6 +42,35 @@ void receive_file(int server_socket)
 
     fclose(file);
 }
+
+bool isValidDate(char *dateStr) {
+    int year, month, day;
+    if (sscanf(dateStr, "%d-%d-%d", &year, &month, &day) != 3) {
+        return false;
+    }
+
+    if (year < 1 || month < 1 || month > 12 || day < 1) {
+        return false;
+    }
+
+    if ((month == 4 || month == 6 || month == 9 || month == 11) && (day > 30)) {
+        return false;
+    }
+
+    if (month == 2) {
+        if ((year % 4 == 0 && year % 100 != 0) || (year % 400 == 0)) {
+            if (day > 29) {
+                return false;
+            }
+        } else {
+            if (day > 28) {
+                return false;
+            }
+        }
+    }
+
+    return true;
+}
 int isFutureDate(char *givenDate)
 {
     const char *dateFormat = "%Y-%m-%d";
@@ -149,6 +178,10 @@ int validateCommand(char *command)
         if (input_date == -1)
         {
             perror("Failed to convert date");
+            return 0;
+        }
+
+        if(!isValidDate(date)){
             return 0;
         }
 
